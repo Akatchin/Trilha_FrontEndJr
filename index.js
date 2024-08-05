9052f1951eee4dc9cb62e1248fcd4ac63b9fdf7a
Thiago Henrique <thiago.jp.221@gmail.com>
2024-08-05 20:47:59 -0300

    Styles 05 and animations

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-window.scroll = ScrollReveal({reset: true});
-console.log('funcionando');
+const displayMessage = document.querySelector('.welcome-message');
 
-scroll.reveal('.profile', {
-    duration: 2000,
-    delay: 500,
-    rotate: {
-        x: 0,
-        y: 500,
-        z: 0 
-    }
-})
+function textAnimation (message, interval) {
+    message.split('').forEach((letter, index) => {
+        setTimeout(() => {
+            displayMessage.innerHTML+=letter;
+        }, index * interval)
+    })
+}
 
-scroll.reveal('.about-reveal', {
-    duration: 2000,
-    distance: '50px'
-})
-
-scroll.reveal('.presentation-text-reveal', {
-    duration: 2000,
-    distance: '90px',
-    origin: 'left'
-}) 
