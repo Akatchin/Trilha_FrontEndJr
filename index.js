4911ee06a072b6da0ea8e8e9a8972b11bfcb44c0
Thiago Henrique <thiago.jp.221@gmail.com>
2024-08-17 00:23:25 -0300

    Responsiveness and finish

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const displayMessage = document.querySelector('.welcome-message');
 
+
 function textAnimation (message, interval) {
     message.split('').forEach((letter, index) => {
         setTimeout(() => {
             displayMessage.innerHTML+=letter;
         }, index * interval)
     })
-}
-
+};
--- reveal.js
+++ reveal.js
@@ -1,6 +1,8 @@
+const displayMessage = document.querySelector('.welcome-message');
+
 window.scroll = ScrollReveal({reset: true});
 
-scroll.reveal('.profile', {
+scroll.reveal('.profile-animation', {
     duration: 2000,
     delay: 500,
     rotate: {
@@ -8,62 +10,63 @@ scroll.reveal('.profile', {
         y: 500,
         z: 0 
     }
-})
+});
 
 scroll.reveal('.about-reveal', {
     duration: 2000,
     distance: '50px'
-})
+});
 
 scroll.reveal('.presentation-text-reveal', {
     duration: 2000,
     distance: '90px',
     origin: 'left'
-}) 
+});
 
 scroll.reveal('.html-icon', {
     duration: 2000,
     distance: '60px',
-})
+});
 
 scroll.reveal('.css-icon', {
     duration: 2000,
     distance: '60px',
     delay: 100
-})
+});
 
 scroll.reveal('.react-icon', {
     duration: 2000,
     distance: '60px',
     delay: 200
-})
+});
 
 scroll.reveal('.javascript-icon', {
     duration: 2000,
     distance: '60px',
     delay: 300
-})
+});
 
 scroll.reveal('.typescript-icon', {
     duration: 2000,
     distance: '60px',
     delay: 400
-})
+});
 
 scroll.reveal('.php-icon', {
     duration: 2000,
     distance: '60px',
     delay: 500
-})
+});
 
 scroll.reveal('.nodejs-icon', {
     duration: 2000,
     distance: '60px',
     delay: 600
-})
+});
 
 scroll.reveal('.mongodb-icon', {
     duration: 2000,
     distance: '60px',
     delay: 700
-})
+});
+
